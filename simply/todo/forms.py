7970bbdff7165e36dcf79dedd23c4bd9a3e27f5f
Bmoore4452 <r.moore803@gmail.com>
2025-03-21 12:08:08 -0400

    created login page

--- simply/todo/forms.py
+++ simply/todo/forms.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from django.forms.widgets import PasswordInput, TextInput
+from django import forms
 
 # Create a User
 
@@ -7,3 +9,9 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+# - Autthenticate a User
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=TextInput())
+    password = forms.CharField(widget=PasswordInput())
--- simply/todo/views.py
+++ simply/todo/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 
-from .forms import CreateUserForm
+from .forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
 
 
 # Create your views here.
 def homepage(request):
     return render(request, "index.html")
 
-
+# Register a User
 def register(request):
     form = CreateUserForm()
     if request.method == "POST":
@@ -15,11 +17,24 @@ def register(request):
         if form.is_valid():
             form.save()
             return redirect
-    context = {'form': form}
+    context = {"form": form}
     return render(request, "register.html", context=context)
 
+# Login a User
+def login(request):
+    form = LoginForm()
+    if request.method == "POST":
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            user = authenticate(request, username=username, password=password)
 
-def login(request): ...
+            if user is not None:
+                auth.login(request, user)
+                return redirect("")
+    context = {"form": form}
+    return render(request, "my-login.html", context=context)
 
 
 def dashboard(request): ...
